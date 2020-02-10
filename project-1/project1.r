e89c6bde9d3678c8e0037aaeac382d264598d962
Fred Lindahl <47213636+lindahlf@users.noreply.github.com>
2020-02-10 16:46:17 +0100

    summarized code for report

--- project-1/project1.r
+++ project-1/project1.r
@@ -8,15 +8,23 @@ SI <- function(men){
 }
 SI_men = SI(fatmen)
 
+#Finding leverage points
+n = nrow(model$model)
+p = ncol(model$model) - 1
+h_ii = lm.influence(model)$hat
+hat_eval <- 2*p/n
+leverage_points <- which(h_ii > hat_eval)
+
+
 #Removing outliers
 fatmen <- fatmen[-c(39),]
-
 
 # Computing a full model
 model = lm(density ~ .-density, data= fatmen) 
 summary(model)
 plot(model)
 
+#Finding best reduced models
 #ols_step_all_possible(model)
 #best_model = ols_step_best_subset(model)
 
@@ -32,6 +40,7 @@ summary(model_5)
 model_6 = lm(density ~ age + weight + abdomen + thigh + forearm + wrist, data = fatmen)
 summary(model_6)
   
+
 
 
 
